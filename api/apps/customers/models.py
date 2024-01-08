1f583817316e277a21c55b65b639e3155b688c2d
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-08 12:47:48 -0300

    fixing email field

--- api/apps/customers/models.py
+++ api/apps/customers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(max_length=30, blank=False)
     cpf = models.CharField(max_length=11, blank=False, unique=True)
     rg = models.CharField(max_length=9, blank=False, unique=True)
     celphone = models.CharField(max_length=14)
