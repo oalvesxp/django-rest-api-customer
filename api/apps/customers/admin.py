31529804abc7ef6aa868872a3f007c8232442e05
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-08 12:34:54 -0300

    adding admin display

--- api/apps/customers/admin.py
+++ api/apps/customers/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from apps.customers.models import Customer
 
-# Register your models here.
+class Customers(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'cpf',
+        'rg',
+        'celphone',
+        'status',
+    )
+    list_display_links = ('id', 'name',)
+    search_fields = ('name',)
+    list_filter = ('status',)
+    list_editable = ('status',)
+    list_per_page = 10
+
+admin.site.register(Customer, Customers)
