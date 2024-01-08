23d8bd63354791b6d280aa716a94c22a3b7427ce
Osvaldo Alves <oalvesxp@gmail.com>
2024-01-08 14:02:12 -0300

    refatoring validation code

--- api/apps/customers/serializer.py
+++ api/apps/customers/serializer.py
@@ -1,27 +1,13 @@
 from rest_framework import serializers
 from apps.customers.models import Customer
+from apps.customers.validators import *
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
     
-    def validate_name(self, name):
-        if not name.isalpha():
-            raise serializers.ValidationError('Este campo não pode conter números.')
-        return name
-    
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError('O CPF deve conter 11 digitos.')
-        return cpf
-    
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError('O RG deve conter 9 digitos.')
-        return rg
-    
-    def validate_celphone(self, celphone):
-        if len(celphone) < 11:
-            raise serializers.ValidationError('O número de celular deve conter o DDD exemplo: 11 99999-9999.')
-        return celphone
+    def validate(self, data):
+        if not validate_cpf(data['cpf']):
+            raise serializers.ValidationError({'cpf':'O CPF deve conter 11 digitos.'})
+        return data
---
+++ api/apps/customers/validators.py
@@ -0,0 +1,23 @@
+def validate_cpf(value):
+    return len(value) == 11
+
+
+# def validate_name(self, name):
+#     if not name.isalpha():
+#         raise serializers.ValidationError('Este campo não pode conter números.')
+#     return name
+
+# def validate_cpf(self, cpf):
+#     if len(cpf) != 11:
+#         raise serializers.ValidationError('O CPF deve conter 11 digitos.')
+#     return cpf
+
+# def validate_rg(self, rg):
+#     if len(rg) != 9:
+#         raise serializers.ValidationError('O RG deve conter 9 digitos.')
+#     return rg
+
+# def validate_celphone(self, celphone):
+#     if len(celphone) < 11:
+#         raise serializers.ValidationError('O número de celular deve conter o DDD exemplo: 11 99999-9999.')
+#     return celphone
