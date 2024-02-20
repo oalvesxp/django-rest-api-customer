de3cba644ff947e56c5c1cade3302e6bb435d881
Osvaldo Alves <oalvesxp@gmail.com>
2024-02-20 11:17:22 -0300

    setuping filter by name for api

--- api/apps/customers/views.py
+++ api/apps/customers/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from apps.customers.serializer import CustomerSerializer
 from apps.customers.models import Customer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CustomersViewset(viewsets.ModelViewSet):
     '''Listando todos os clientes'''
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ['name']
