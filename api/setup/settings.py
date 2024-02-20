bb9628d4574c53b8927b59e0424a6ba0c1148cb6
Osvaldo Alves <oalvesxp@gmail.com>
2024-02-20 11:07:55 -0300

    changing method to select pagination

--- api/setup/settings.py
+++ api/setup/settings.py
@@ -126,6 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Pagination
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
